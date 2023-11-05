761e8073e7c175b6208e0112703f8b5350c81017
Alejandro <sastre212324@gmail.com>
2023-11-05 15:35:55 +0100

    Quinto commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     routes: {
-        '/': 'about.astro', // Esto establece index.astro como la página de inicio
+        '/': 'index.astro', // Esto establece index.astro como la página de inicio
       },
 });
 
-//fvKuujPyqJwYfeToP16jxt9wHGqHHuYguJbO15IC
