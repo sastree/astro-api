586cd435f777a45452bcc1b3495aeb2223bfb886
Alejandro <sastre212324@gmail.com>
2023-11-06 13:44:03 +0100

    Octavo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     routes: {
-        '/': 'index.astro', // Esto establece index.astro como la página de inicio
+        '/': 'index.astro', // Esto establece index.astro como la página de inicio que usa Netlify
       },
 });
 
